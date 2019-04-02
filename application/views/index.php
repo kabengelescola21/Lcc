1aae1b8a4a1865116a1126f08f1dfcb11e4f834d
Simon MWEPU <simonmwepu@gmail.com>
2019-04-02 16:25:42 +0200

    [UPDATE]sytle link

--- application/views/index.php
+++ application/views/index.php
@@ -11,7 +11,7 @@
 <meta name="author" content="jThemes Studio">
 <title>imEvent EM - Event Conference Landing Page</title>
 <link rel="stylesheet" type="text/css" href="<?= base_url() ?>splash/css/bootstrap.css" media="all">
-<link rel="stylesheet" type="text/css" href="splash/css/style.css" media="all">
+<link rel="stylesheet" type="text/css" href="<?= base_url() ?>splash/css/style.css" media="all">
 <link rel="stylesheet" type="text/css" href="splash/css/font-awesome.min.css" media="all">
 <link type="text/css" media="screen" rel="stylesheet" href="splash/awwwards.css" />
 <!--[if lt IE 9]>
