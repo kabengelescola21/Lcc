6bd516396117e67b75c01ee91b0ad17f106deb60
Atthie <atthie27@gmail.com>
2019-04-03 10:03:52 +0200

    [update]put a comment

--- application/views/index.php
+++ application/views/index.php
@@ -99,150 +99,149 @@
         <div id="main">
         <!-- SLIDER -->
         <section class="page-section no-padding background-img-slider">
+            <!-- home (slide)-->
             <div class="container">
+                <div id="main-slider" class="owl-carousel owl-theme">
 
-            <div id="main-slider" class="owl-carousel owl-theme">
-
-                <!-- Slide -->
-                <div class="item page text-center slide0">
-                    <div class="caption">
-                        <div class="container">
-                            <div class="div-table">
-                                <div class="div-cell">
-                                <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
-                                <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
-                                    <div class="countdown-wrapper">
-                                        <div id="defaultCountdown" class="defaultCountdown clearfix"></div>
-                                    </div>
-                                    <p class="caption-text">
-                                        <a class="btn btn-theme btn-theme-xl scroll-to" href="#register" data-animation="flipInY" data-animation-delay="600"> Register <i class="fa fa-arrow-circle-right"></i></a>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Slide -->
-                <div class="item page text-center slide1">
-                    <div class="caption">
-                    <div class="container">
-                        <div class="div-table">
-                        <div class="div-cell">
-                            <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
-                            <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
-                            <p class="caption-text">
-                                <a class="btn btn-theme btn-theme-xl scroll-to" href="#register" data-animation="flipInY" data-animation-delay="600"> Register <i class="fa fa-arrow-circle-right"></i></a><!--
-                                --><a class="btn btn-theme btn-theme-xl btn-theme-transparent-white" href="http://www.youtube.com/watch?v=O-zpOMYRi0w" data-gal="prettyPhoto" data-animation="flipInY" data-animation-delay="900">Watch video</a>
-                            </p>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-
-                <!-- Slide -->
-                <!-- <div class="item page slide2">
-                    <div class="caption">
-                    <div class="container">
-                        <div class="div-table">
-                        <div class="div-cell">
-                            <div class="row">
-                                <div class="col-md-6 col-lg-4">
-                                    <div class="form-background">
-                                    <div class="form-header color">
-                                        <h1 class="section-title">
-                                            <span class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-ticket fa-stack-1x"></i></span></span>
-                                            <span class="title-inner">Event Register</span>
-                                        </h1>
-                                    </div>
-
-                                     <form id="registration-form-alt" name="registration-form-alt" class="registration-form alt" action="#" method="post">
-                                        <div class="row">
-                                            <div class="col-sm-12 form-alert"></div>
-                                            <div class="col-sm-12">
-                                                <div class="form-group">
-                                                    <input
-                                                            type="text" class="form-control input-name"
-                                                            data-toggle="tooltip" title="Name is required"
-                                                            placeholder="Name and Surname"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12">
-                                                <div class="form-group">
-                                                    <input
-                                                            type="text" class="form-control input-email"
-                                                            data-toggle="tooltip" title="Mail is required"
-                                                            placeholder="Your Mail Here"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12">
-                                                <div class="form-group">
-                                                    <input
-                                                            type="text" class="form-control input-phone"
-                                                            placeholder="Your Phone Number"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12">
-                                                <div class="form-group selectpicker-wrapper">
-                                                    <select
-                                                            class="selectpicker input-price" data-live-search="false" data-width="100%"
-                                                            data-toggle="tooltip" title="Select Your Price Tab">
-                                                        <option>Select Your Price Tab</option>
-                                                        <option>$100</option>
-                                                        <option>$200</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-12">
-                                                <div class="text-center">
-                                                    <button
-                                                            data-animation="flipInY" data-animation-delay="100"
-                                                            class="btn btn-theme btn-block submit-button" type="submit"
-                                                            > Register Now <i class="fa fa-arrow-circle-right"></i></button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form> 
-                                    </div>
-                                </div> 
-                                <div class="col-md-6 col-lg-8">
-                                    <div class="text-holder">
+                    <!-- Slide -->
+                    <div class="item page text-center slide0">
+                        <div class="caption">
+                            <div class="container">
+                                <div class="div-table">
+                                    <div class="div-cell">
                                     <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
                                     <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
+                                        <div class="countdown-wrapper">
+                                            <div id="defaultCountdown" class="defaultCountdown clearfix"></div>
+                                        </div>
+                                        <p class="caption-text">
+                                            <a class="btn btn-theme btn-theme-xl scroll-to" href="#register" data-animation="flipInY" data-animation-delay="600"> Register <i class="fa fa-arrow-circle-right"></i></a>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
-                            <!-- Event description -->
-                            <!-- /Event description -->
-                        </div>
                         </div>
                     </div>
-                    </div>
-                </div> -->
 
-                <!-- Slide -->
-                <div class="item page text-center slide3">
-                    <div class="caption">
-                    <div class="container">
-                        <div class="div-table">
-                        <div class="div-cell">
-                        <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
-                            <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
-                            <p class="caption-text">
-                                <a class="btn btn-play" href="http://www.youtube.com/watch?v=O-zpOMYRi0w" data-gal="prettyPhoto"><i class="fa fa-play"></i></a>
-                            </p>
-                            <!-- Event description -->
-                            <!-- /Event description -->
+                    <!-- Slide -->
+                    <div class="item page text-center slide1">
+                        <div class="caption">
+                        <div class="container">
+                            <div class="div-table">
+                            <div class="div-cell">
+                                <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
+                                <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
+                                <p class="caption-text">
+                                    <a class="btn btn-theme btn-theme-xl scroll-to" href="#register" data-animation="flipInY" data-animation-delay="600"> Register <i class="fa fa-arrow-circle-right"></i></a><!--
+                                    --><a class="btn btn-theme btn-theme-xl btn-theme-transparent-white" href="http://www.youtube.com/watch?v=O-zpOMYRi0w" data-gal="prettyPhoto" data-animation="flipInY" data-animation-delay="900">Watch video</a>
+                                </p>
+                            </div>
+                            </div>
                         </div>
                         </div>
                     </div>
+
+                    <!-- Slide -->
+                    <!-- <div class="item page slide2">
+                        <div class="caption">
+                        <div class="container">
+                            <div class="div-table">
+                            <div class="div-cell">
+                                <div class="row">
+                                    <div class="col-md-6 col-lg-4">
+                                        <div class="form-background">
+                                        <div class="form-header color">
+                                            <h1 class="section-title">
+                                                <span class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-ticket fa-stack-1x"></i></span></span>
+                                                <span class="title-inner">Event Register</span>
+                                            </h1>
+                                        </div>
+
+                                        <form id="registration-form-alt" name="registration-form-alt" class="registration-form alt" action="#" method="post">
+                                            <div class="row">
+                                                <div class="col-sm-12 form-alert"></div>
+                                                <div class="col-sm-12">
+                                                    <div class="form-group">
+                                                        <input
+                                                                type="text" class="form-control input-name"
+                                                                data-toggle="tooltip" title="Name is required"
+                                                                placeholder="Name and Surname"/>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-12">
+                                                    <div class="form-group">
+                                                        <input
+                                                                type="text" class="form-control input-email"
+                                                                data-toggle="tooltip" title="Mail is required"
+                                                                placeholder="Your Mail Here"/>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-12">
+                                                    <div class="form-group">
+                                                        <input
+                                                                type="text" class="form-control input-phone"
+                                                                placeholder="Your Phone Number"/>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-12">
+                                                    <div class="form-group selectpicker-wrapper">
+                                                        <select
+                                                                class="selectpicker input-price" data-live-search="false" data-width="100%"
+                                                                data-toggle="tooltip" title="Select Your Price Tab">
+                                                            <option>Select Your Price Tab</option>
+                                                            <option>$100</option>
+                                                            <option>$200</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-12">
+                                                    <div class="text-center">
+                                                        <button
+                                                                data-animation="flipInY" data-animation-delay="100"
+                                                                class="btn btn-theme btn-block submit-button" type="submit"
+                                                                > Register Now <i class="fa fa-arrow-circle-right"></i></button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form> 
+                                        </div>
+                                    </div> 
+                                    <div class="col-md-6 col-lg-8">
+                                        <div class="text-holder">
+                                        <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
+                                        <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Event description -->
+                                <!-- /Event description -->
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div> -->
+
+                    <!-- Slide -->
+                    <div class="item page text-center slide3">
+                        <div class="caption">
+                        <div class="container">
+                            <div class="div-table">
+                            <div class="div-cell">
+                            <h2 class="caption-title" data-animation="fadeInDown" data-animation-delay="100"><span>Avril 17-19, 2019</span></h2>
+                                <h3 class="caption-subtitle" data-animation="fadeInUp" data-animation-delay="300">Semaine du rire</h3>
+                                <p class="caption-text">
+                                    <a class="btn btn-play" href="http://www.youtube.com/watch?v=O-zpOMYRi0w" data-gal="prettyPhoto"><i class="fa fa-play"></i></a>
+                                </p>
+                                <!-- Event description -->
+                                <!-- /Event description -->
+                            </div>
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
-            </div>
-
+            <!-- end home(slide)-->
             <!-- Event description -->
             <div class="event-description">
                 <div class="container">
