d110861fd584bf039f5ac0729592383c7cd4cea5
Simon MWEPU <simonmwepu@gmail.com>
2019-04-03 11:51:47 +0200

    [ADD]OSM canvas

--- application/views/index.php
+++ application/views/index.php
@@ -2849,21 +2849,23 @@
                     <div class="on-gmap color">
                         <h1 class="section-title">
                             <span data-animation="flipInY" data-animation-delay="100" class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-ticket fa-stack-1x"></i></span></span>
-                            <span data-animation="fadeInRight" data-animation-delay="100" class="title-inner">Event Location</span>
+                            <span data-animation="fadeInRight" data-animation-delay="100" class="title-inner">Localiser un evenement</span>
                         </h1>
-                        <p data-animation="fadeInUp" data-animation-delay="200" class="text-uppercase">Apple Store SOHO‎ <br/>
-                            103 Prince St New York, <br/>
-                            NY 10012, United States <br/>
-                            +1 212-226-3126</p>
-                        <p><a href="mailto:youremail@domain.com">hello@imevent.com</a></p>
+                        <p data-animation="fadeInUp" data-animation-delay="200" class="text-uppercase">Soirée du rire <br/>
+                            103,Mwepu<br/>
+                            +243 99 </p>
+                        <p><a href="mailto:youremail@domain.com">itot-africa.com</a></p>
                         <a href="#" class="btn btn-theme"
-                           data-animation="flipInY" data-animation-delay="300">Get Direction <i class="fa fa-arrow-circle-right"></i></a>
+                           data-animation="flipInY" data-animation-delay="300">Obtenir la Direction <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
 
                 <!-- Google map -->
                 <div class="google-map">
                     <div id="map-canvas"></div>
+
+                    <div id="my_osm_widget_map"></div>
+                    
                 </div>
                 <!-- /Google map -->
 
