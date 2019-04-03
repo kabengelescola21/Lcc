aeb0ead08f519f7c6f372383b03aec63072de8b0
Simon MWEPU <simonmwepu@gmail.com>
2019-04-03 11:56:37 +0200

    [ADD] location OSM

--- application/views/_inc/footer.php
+++ application/views/_inc/footer.php
@@ -56,6 +56,24 @@
 
 <script type="text/javascript">
 
+    var mymap = L.map('my_osm_widget_map', { /* use the same name as your <div id=""> */
+    center: [XXX-LATITUDE-CENTER-XXX, XXX-LONGITUDE-CENTER-XXX], /* set GPS Coordinates */
+    zoom: 17, /* define the zoom level */
+    zoomControl: false, /* false = no zoom control buttons displayed */
+    scrollWheelZoom: false /* false = scrolling zoom on the map is locked */
+    });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=XXX-YOUR-MAPBOX-ACCESS-TOKEN-XXX', { /* set your personal MapBox Access Token */
+    maxZoom: 20, /* zoom limit of the map */
+    attribution: 'Données &copy; Contributeurs <a href="http://openstreetmap.org">OpenStreetMap</a> + ' +
+        '<a href="http://mapbox.com">Mapbox</a> | ' +
+        '<a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a> ' +
+        'Guillaume Rouan 2016', /* set the map's caption */
+        id: 'mapbox.streets' /* mapbox.light / dark / streets / outdoors / satellite */
+    }).addTo(mymap);
+
+    L.marker([XXX-LATITUDE-XXX, XXX-LONGITUDE-XXX]).addTo(mymap); /* set your location's GPS Coordinates : [LAT,LON] */
+
     jQuery(document).ready(function () {
         theme.init();
         theme.initMainSlider();
