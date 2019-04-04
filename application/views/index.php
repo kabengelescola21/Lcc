e1fc4334d00ded00864ed754946db4764a9e7a3d
kabengeleScola21 <scola0021@gmail.com>
2019-04-04 16:17:24 +0200

    [UPDATE]translation

--- application/views/index.php
+++ application/views/index.php
@@ -229,7 +229,7 @@
             <div class="container">
                 <h1 class="section-title">
                     <span data-animation="flipInY" data-animation-delay="300" class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-ticket fa-stack-1x"></i></span></span>
-                    <span data-animation="fadeInRight" data-animation-delay="500" class="title-inner">Donation <small> / lorem ipsum</small></span>
+                    <span data-animation="fadeInRight" data-animation-delay="500" class="title-inner">Don <small> / vous pouvez soutenir vos artistes</small></span>
                 </h1>
                 <p>Etiam molestie, quam eget dignissim dapibus, diam libero auctor justo, a eleifend urna tellus et ligula. Curabitur elementum diam nec lacus pretium.</p>
 
@@ -247,7 +247,7 @@
                     <input type="hidden" name="amount" value="25.00">
                     <input type="hidden" name="currency_code" value="USD">
                     <!-- Display the payment button. -->
-                    <button name="submit" class="btn btn-theme">Make a donation now</button>
+                    <button name="submit" class="btn btn-theme">faire un don maintenant</button>
                     <img alt="" border="0" width="1" height="1" src="../../../www.paypalobjects.com/en_US/i/scr/pixel.gif">
                 </form>
 
