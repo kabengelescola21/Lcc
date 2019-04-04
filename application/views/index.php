346ef4b084df43c42491849f5ac0a581d67d9a8b
Simon MWEPU <simonmwepu@gmail.com>
2019-04-04 22:31:57 +0200

    [DELETE]blog session 'cause  it's less important

--- application/views/index.php
+++ application/views/index.php
@@ -2612,129 +2612,6 @@
             </div>
         </section>
         <!-- /Donation -->
-        <section class="page-section">
-            <div class="container">
-                <h1 class="section-title">
-                    <span data-animation="flipInY" data-animation-delay="300" class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-file-text-o fa-stack-1x"></i></span></span>
-                    <span data-animation="fadeInRight" data-animation-delay="500" class="title-inner">Recent Blog Posts <small> / get news!</small></span>
-                </h1>
-                <div class="row post-row">
-
-                    <!-- -->
-                    <div class="col-md-4">
-                        <article class="post-wrap" data-animation="fadeInUp" data-animation-delay="100">
-                            <div class="post-media">
-                                <div class="post-type">
-                                    <i class="fa fa-video-camera"></i>
-                                </div>
-                                <img src="assets/img/preview/recent-post-1.jpg" alt=""/>
-                            </div>
-                            <div class="post-header">
-                                <h2 class="post-title"><a href="#">Standart Blog Post Header Here</a></h2>
-                                <div class="post-meta">
-                                    <span class="post-date">
-                                        Posted on
-                                        <span class="day">17th</span>
-                                        <span class="month">May</span>
-                                        <span class="year">2015</span>
-                                    </span>
-                                    <span class="pull-right">
-                                        <i class="fa fa-comment"></i> <a href="#">12</a>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="post-body">
-                                <div class="post-excerpt">
-                                    <p>Fusce pellentesque velvitae tincidunt egestas. Pellentesque habitant morbi tristiquetus et senectus et netus et malesuada ac turpis.</p>
-                                </div>
-                            </div>
-                            <div class="post-footer">
-                                <span class="post-readmore">
-                                    <a href="#" class="btn btn-theme btn-theme-transparent">Read more</a>
-                                </span>
-                            </div>
-                        </article>
-                    </div>
-
-                    <!-- -->
-                    <div class="col-md-4">
-                        <article class="post-wrap" data-animation="fadeInUp" data-animation-delay="300">
-                            <div class="post-media">
-                                <div class="post-type">
-                                    <i class="fa fa-photo"></i>
-                                </div>
-                                <img src="assets/img/preview/recent-post-2.jpg" alt=""/>
-                            </div>
-                            <div class="post-header">
-                                <h2 class="post-title"><a href="#">Standart Blog Post Header Here</a></h2>
-                                <div class="post-meta">
-                                    <span class="post-date">
-                                        Posted on
-                                        <span class="day">17th</span>
-                                        <span class="month">May</span>
-                                        <span class="year">2015</span>
-                                    </span>
-                                    <span class="pull-right">
-                                        <i class="fa fa-comment"></i> <a href="#">12</a>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="post-body">
-                                <div class="post-excerpt">
-                                    <p>Fusce pellentesque velvitae tincidunt egestas. Pellentesque habitant morbi tristiquetus et senectus et netus et malesuada ac turpis.</p>
-                                </div>
-                            </div>
-                            <div class="post-footer">
-                                <span class="post-readmore">
-                                    <a href="#" class="btn btn-theme btn-theme-transparent">Read more</a>
-                                </span>
-                            </div>
-                        </article>
-                    </div>
-
-                    <!-- -->
-                    <div class="col-md-4">
-                        <article class="post-wrap" data-animation="fadeInUp" data-animation-delay="500">
-                            <div class="post-media">
-                                <div class="post-type">
-                                    <i class="fa fa-music"></i>
-                                </div>
-                                <img src="assets/img/preview/recent-post-3.jpg" alt=""/>
-                            </div>
-                            <div class="post-header">
-                                <h2 class="post-title"><a href="#">Standart Blog Post Header Here</a></h2>
-                                <div class="post-meta">
-                                    <span class="post-date">
-                                        Posted on
-                                        <span class="day">17th</span>
-                                        <span class="month">May</span>
-                                        <span class="year">2014</span>
-                                    </span>
-                                    <span class="pull-right">
-                                        <i class="fa fa-comment"></i> <a href="#">12</a>
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="post-body">
-                                <div class="post-excerpt">
-                                    <p>Fusce pellentesque velvitae tincidunt egestas. Pellentesque habitant morbi tristiquetus et senectus et netus et malesuada ac turpis.</p>
-                                </div>
-                            </div>
-                            <div class="post-footer">
-                                <span class="post-readmore">
-                                    <a href="#" class="btn btn-theme btn-theme-transparent">Read more</a>
-                                </span>
-                            </div>
-                        </article>
-                    </div>
-
-                </div>
-                <div class="text-center margin-top">
-                    <a data-animation="flipInY" data-animation-delay="100" href="#" class="btn btn-theme"><i class="fa fa-file"></i> See all news</a>
-                </div>
-            </div>
-        </section>
-
         <!-- PAGE LOCATION -->
         <section class="page-section" id="location">
             <div class="container full-width gmap-background">
@@ -2766,12 +2643,12 @@
         <!-- /PAGE LOCATION -->
 
         <!-- PAGE CONTACT -->
-        <section class="page-section color">
+        <section class="page-section color contactez-nous">
             <div class="container">
 
                 <h1 class="section-title">
                     <span data-animation="flipInY" data-animation-delay="100" class="icon-inner"><span class="fa-stack"><i class="fa rhex fa-stack-2x"></i><i class="fa fa-ticket fa-stack-1x"></i></span></span>
-                    <span data-animation="fadeInRight" data-animation-delay="100" class="title-inner">Contactez-nous<small>/ lorem ipsum</small></span>
+                    <span data-animation="fadeInRight" data-animation-delay="100" class="title-inner">Contactez-nous<small></small></span><br>
                 </h1>
 
                 <!-- Contact form -->
@@ -2780,7 +2657,7 @@
                     <div class="col-sm-12 af-outer af-required">
                         <div class="form-group af-inner">
                             <input
-                                    type="text" name="name" id="name" placeholder="Type Your Name..." value="" size="30"
+                                    type="text" name="name" id="name" placeholder="Votre nom" value="" size="30"
                                     data-toggle="tooltip" title="Name is required"
                                     class="form-control placeholder"/>
                         </div>
@@ -2789,7 +2666,7 @@
                     <div class="col-sm-12 af-outer af-required">
                         <div class="form-group af-inner">
                             <input
-                                    type="text" name="email" id="email" placeholder="Type Your Email..." value="" size="30"
+                                    type="text" name="email" id="email" placeholder="Votre email" value="" size="30"
                                     data-toggle="tooltip" title="Email is required"
                                     class="form-control placeholder"/>
                         </div>
@@ -2798,7 +2675,7 @@
                     <div class="col-sm-12 af-outer af-required">
                         <div class="form-group af-inner">
                             <textarea
-                                    name="message" id="input-message" placeholder="Type Your Message..." rows="4" cols="50"
+                                    name="message" id="input-message" placeholder="Votre message" rows="4" cols="50"
                                     data-toggle="tooltip" title="Message is required"
                                     class="form-control placeholder"></textarea>
                         </div>
@@ -2806,7 +2683,7 @@
 
                     <div class="col-sm-12 af-outer af-required text-center">
                         <div class="form-group af-inner">
-                            <input type="submit" name="submit" class="form-button form-button-submit btn btn-theme btn-theme-lg btn-theme-transparent" id="submit_btn" value="Send message" />
+                            <input type="submit" name="submit" class="form-button form-button-submit btn btn-theme btn-theme-lg btn-theme-transparent" id="submit_btn" value="Envoyer" />
                         </div>
                     </div>
 
